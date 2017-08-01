8320918bd950fe9111ffab88d241ac22d002ef21
unknown <claudiucelfilip@gmail.com>
2017-08-01 14:53:35 +0300

    restored angular functionality and removed build folders

--- AngularUniversal/Scripts/angular/server.js
+++ AngularUniversal/Scripts/angular/server.js
@@ -1,33 +1,11 @@
 "use strict";
-require('zone.js/dist/zone-node');
-require('reflect-metadata');
-require('rxjs/Rx');
-var core_1 = require('@angular/core');
 var aspnet_prerendering_1 = require('aspnet-prerendering');
-var aspnetcore_engine_1 = require('@nguniversal/aspnetcore-engine');
-var app_server_module_1 = require('./app/app.server.module');
-core_1.enableProdMode();
-function createTransferScript(transferData) {
-    return "<script>window['TRANSFER_CACHE'] = " + JSON.stringify(transferData) + ";</script>";
-}
-;
-;
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = aspnet_prerendering_1.createServerRenderer(function (params) {
-    var setupOptions = {
-        appSelector: '<app></app>',
-        ngModule: app_server_module_1.AppServerModule,
-        request: params,
-        providers: []
-    };
-    return aspnetcore_engine_1.ngAspnetCoreEngine(setupOptions).then(function (response) {
-        response.globals.transferData = createTransferScript({
-            someData: 'Transfer this to the client on the window.TRANSFER_CACHE {} object',
-            fromDotnet: params.data.thisCameFromDotNET
-        });
-        return ({
-            html: response.html,
-            globals: response.globals
+exports.default = aspnet_prerendering_1.createServerRenderer(function () {
+    return new Promise(function (resolve) {
+        return resolve({
+            html: "hello world",
+            globals: {}
         });
     });
 });
--- AngularUniversal/Scripts/angular/server.ts
+++ AngularUniversal/Scripts/angular/server.ts
@@ -1,81 +1,81 @@
-//import 'zone.js/dist/zone-node';
-//import 'reflect-metadata';
-//import 'rxjs/Rx';
+import 'zone.js/dist/zone-node';
+import 'reflect-metadata';
+import 'rxjs/Rx';
 
-//import { Type, NgModuleFactory, Provider, enableProdMode } from '@angular/core';
-//import { INITIAL_CONFIG } from '@angular/platform-server';
-//import { createServerRenderer, RenderResult } from 'aspnet-prerendering';
+import { Type, NgModuleFactory, Provider, enableProdMode } from '@angular/core';
+import { INITIAL_CONFIG } from '@angular/platform-server';
+import { createServerRenderer, RenderResult } from 'aspnet-prerendering';
 
-//import { ngAspnetCoreEngine } from '@nguniversal/aspnetcore-engine';
+import { ngAspnetCoreEngine } from '@nguniversal/aspnetcore-engine';
 
-//import { AppServerModule } from './app/app.server.module';
+import { AppServerModule } from './app/app.server.module';
 
-//enableProdMode();
+enableProdMode();
 
-//function createTransferScript(transferData: Object): string {
-//    return `<script>window['TRANSFER_CACHE'] = ${JSON.stringify(transferData)};</script>`;
-//}
+function createTransferScript(transferData: Object): string {
+    return `<script>window['TRANSFER_CACHE'] = ${JSON.stringify(transferData)};</script>`;
+}
 
-//interface IEngineOptions {
-//    appSelector: string;
-//    request: IRequestParams;
-//    ngModule: Type<{}> | NgModuleFactory<{}>;
-//    providers?: Provider[];
-//};
+interface IEngineOptions {
+    appSelector: string;
+    request: IRequestParams;
+    ngModule: Type<{}> | NgModuleFactory<{}>;
+    providers?: Provider[];
+};
 
-//interface IRequestParams {
-//    location: any;              // e.g., Location object containing information '/some/path'
-//    origin: string;             // e.g., 'https://example.com:1234'
-//    url: string;                // e.g., '/some/path'
-//    baseUrl: string;            // e.g., '' or '/myVirtualDir'
-//    absoluteUrl: string;        // e.g., 'https://example.com:1234/some/path'
-//    domainTasks: Promise<any>;
-//    data: any;                  // any custom object passed through from .NET
-//};
+interface IRequestParams {
+    location: any;              // e.g., Location object containing information '/some/path'
+    origin: string;             // e.g., 'https://example.com:1234'
+    url: string;                // e.g., '/some/path'
+    baseUrl: string;            // e.g., '' or '/myVirtualDir'
+    absoluteUrl: string;        // e.g., 'https://example.com:1234/some/path'
+    domainTasks: Promise<any>;
+    data: any;                  // any custom object passed through from .NET
+};
 
-//export default createServerRenderer(params => {
-//    const setupOptions: IEngineOptions = {
-//        appSelector: '<app></app>',
-//        ngModule: AppServerModule,
-//        request: params,
-//        providers: [
-//            /* Other providers you want to pass into the App would go here
-//            *    { provide: CookieService, useClass: ServerCookieService }
+export default createServerRenderer(params => {
+    const setupOptions: IEngineOptions = {
+        appSelector: '<app></app>',
+        ngModule: AppServerModule,
+        request: params,
+        providers: [
+            /* Other providers you want to pass into the App would go here
+            *    { provide: CookieService, useClass: ServerCookieService }
     
-//            * ie: Just an example of Dependency injecting a Class for providing Cookies (that you passed down from the server)
-//              (Where on the browser you'd have a different class handling cookies normally)
-//            */
-//        ]
-//    };
+            * ie: Just an example of Dependency injecting a Class for providing Cookies (that you passed down from the server)
+              (Where on the browser you'd have a different class handling cookies normally)
+            */
+        ]
+    };
 
-//    return ngAspnetCoreEngine(setupOptions).then(response => {
+    return ngAspnetCoreEngine(setupOptions).then(response => {
 
-//        // Want to transfer data from Server -> Client?
+        // Want to transfer data from Server -> Client?
 
-//        // Add transferData to the response.globals Object, and call createTransferScript({}) passing in the Object key/values of data
-//        // createTransferScript() will JSON Stringify it and return it as a <script> window.TRANSFER_CACHE={}</script>
-//        // That your browser can pluck and grab the data from
-//        response.globals.transferData = createTransferScript({
-//            someData: 'Transfer this to the client on the window.TRANSFER_CACHE {} object',
-//            fromDotnet: params.data.thisCameFromDotNET // example of data coming from dotnet, in HomeController
-//        });
-
-//        return ({
-//            html: response.html,
-//            globals: response.globals
-//        });
-
-//    });
-//});
-
-
-import { createServerRenderer } from 'aspnet-prerendering';
-
-export default createServerRenderer(function () {
-    return new Promise((resolve) => {
-        return resolve({
-            html: "hello world",
-            globals: {}
+        // Add transferData to the response.globals Object, and call createTransferScript({}) passing in the Object key/values of data
+        // createTransferScript() will JSON Stringify it and return it as a <script> window.TRANSFER_CACHE={}</script>
+        // That your browser can pluck and grab the data from
+        response.globals.transferData = createTransferScript({
+            someData: 'Transfer this to the client on the window.TRANSFER_CACHE {} object',
+            fromDotnet: params.data.thisCameFromDotNET // example of data coming from dotnet, in HomeController
         });
+
+        return ({
+            html: response.html,
+            globals: response.globals
+        });
+
     });
 });
+
+
+//import { createServerRenderer } from 'aspnet-prerendering';
+
+//export default createServerRenderer(function () {
+//    return new Promise((resolve) => {
+//        return resolve({
+//            html: "hello world",
+//            globals: {}
+//        });
+//    });
+//});
